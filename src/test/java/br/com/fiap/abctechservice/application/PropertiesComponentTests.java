a3bc48618e4e4f2304ee1e13d151302fa3b01745
Afonso Gomes <afonso.gomes@vrsoft.com.br>
2022-04-07 18:22:49 -0300

    Ajustes testes

--- src/test/java/br/com/fiap/abctechservice/application/PropertiesComponentTests.java
+++ src/test/java/br/com/fiap/abctechservice/application/PropertiesComponentTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class PropertiesComponentTest {
+public class PropertiesComponentTests {
 
     private PropertiesComponent propertiesComponent;
 
